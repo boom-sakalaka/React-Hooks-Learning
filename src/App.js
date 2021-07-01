b9b713ce9541344dc848f87201f1563c36c14c7e
gzh <936285702@qq.com>
2021-07-01 11:55:49 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-06-30 15:54:49
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-07-01 11:26:21
+ * @FilePath: \my-app\src\App.js
+ */
+import './App.css';
+// import Counter from './pages/Counter';
+// import MemoTest from './pages/MemoTest';
+// import UseReducerTest from './pages/UseReducerTest'
+// import UseLayoutEffect from './pages/UseLayoutEffect';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <Counter number={3}/> */}
+
+      {/* memo useMemo useCallback 的使用  加上了useFomatList的使用，配合useMemo useCallBack 提高性能*/}
+      {/* <MemoTest /> */}
+
+      {/* UseReducer 的使用 类似于redux */}
+      {/* <UseReducerTest /> */}
+
+      {/* <UseLayoutEffect /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/UseLayoutEffect.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: GZH
+ * @Date: 2021-06-30 17:03:42
+ * @LastEditors: GZH
+ * @LastEditTime: 2021-07-01 11:32:58
+ * 执行时机不同，useLayoutEffect 是修改数据后，组件未发生渲染的时候
+ * @FilePath: \my-app\src\pages\UseLayoutEffect.js
+ */
+import { useLayoutEffect, useEffect, useState } from 'react';
+export default function LayoutEffect() {
+  const [color, setColor] = useState('red');
+  useLayoutEffect(() => {
+    alert(color);
+  });
+  useEffect(() => {
+    console.log('color', color);
+  });
+  return (
+    <>
+      <div id='myDiv' style={{ background: color }}>
+        颜色
+      </div>
+      <button onClick={() => setColor('red')}>红</button>
+      <button onClick={() => setColor('yellow')}>黄</button>
+      <button onClick={() => setColor('blue')}>蓝</button>
+    </>
+  );
+}
