3c14d2b3ed1fbbaae9012d550d35caa49afe0fbe
gzh <936285702@qq.com>
2021-07-15 10:35:21 +0800

    react 对state 修改的优化，合并state的修改

--- src/pages/state/index.js
+++ src/pages/state/index.js
@@ -2,12 +2,16 @@
  * @Author: GZH
  * @Date: 2021-07-15 09:38:52
  * @LastEditors: GZH
- * @LastEditTime: 2021-07-15 10:32:52
+ * @LastEditTime: 2021-07-15 10:35:16
  * @FilePath: \my-app\src\pages\state\index.js
  * @Description:
  * 学习笔记
  * https://segmentfault.com/a/1190000039417644
  * https://juejin.cn/book/6945998773818490884/section/6951186955321376775
+ *
+ *
+ * 对比 函数组件的useState 和类组件的setState, 如果子组件没有加上 pureComponent ，那么不管怎样都会更新子组件
+ * 函数组件会有一个默认对比的过程
  */
 import React, { useEffect, useState } from 'react';
 // import ReactDOM from 'react-dom';
