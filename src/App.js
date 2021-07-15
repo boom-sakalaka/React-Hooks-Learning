78e18a17d08214ff4bc08ce8c70f9c30fde2133f
gzh <936285702@qq.com>
2021-07-15 09:22:10 +0800

    React jsx的理解

--- src/App.js
+++ src/App.js
@@ -2,37 +2,35 @@
  * @Author: GZH
  * @Date: 2021-06-30 15:54:49
  * @LastEditors: GZH
- * @LastEditTime: 2021-07-02 14:40:51
+ * @LastEditTime: 2021-07-15 09:15:03
  * @FilePath: \my-app\src\App.js
  */
 import './App.css';
 // import Counter from './pages/Counter';
 // import MemoTest from './pages/MemoTest';
 // import UseReducerTest from './pages/UseReducerTest'
-// import UseLayoutEffect from './pages/UseLayoutEffect';
+// import UseLayoutEffect from './pages/base/UseLayoutEffect';
 // import MousePosition from './pages/MousePosition';
 // import ParentCom from './pages/memo/ParentCom';
 // import ParentComTwo from './pages/memo/ParentComTwo';
-import ParentComThree from './pages/memo/ParentComThree';
+// import ParentComThree from './pages/memo/ParentComThree';
+import JsxSource from './pages/jsxSource/index';
 
 function App() {
   return (
     <div className='App'>
       {/* <Counter number={3}/> */}
-
       {/* memo useMemo useCallback 的使用  加上了useFomatList的使用，配合useMemo useCallBack 提高性能*/}
       {/* <MemoTest /> */}
-
       {/* UseReducer 的使用 类似于redux */}
       {/* <UseReducerTest /> */}
-
       {/* <UseLayoutEffect /> */}
-
       {/* <MousePosition /> */}
-
       {/* <ParentCom /> */}
+      {/* <ParentComThree /> */}
 
-      <ParentComThree />
+      {/* jsx 解析原理 */}
+      <JsxSource />
     </div>
   );
 }
--- src/pages/base/UseLayoutEffect.js
+++ src/pages/base/UseLayoutEffect.js
@@ -2,19 +2,19 @@
  * @Author: GZH
  * @Date: 2021-06-30 17:03:42
  * @LastEditors: GZH
- * @LastEditTime: 2021-07-01 11:32:58
+ * @LastEditTime: 2021-07-13 17:38:17
  * 执行时机不同，useLayoutEffect 是修改数据后，组件未发生渲染的时候
- * @FilePath: \my-app\src\pages\UseLayoutEffect.js
+ * @FilePath: \my-app\src\pages\base\UseLayoutEffect.js
  */
 import { useLayoutEffect, useEffect, useState } from 'react';
 export default function LayoutEffect() {
   const [color, setColor] = useState('red');
   useLayoutEffect(() => {
-    alert(color);
+    // alert(color);
   });
   useEffect(() => {
     console.log('color', color);
-  });
+  }, [color]);
   return (
     <>
       <div id='myDiv' style={{ background: color }}>
